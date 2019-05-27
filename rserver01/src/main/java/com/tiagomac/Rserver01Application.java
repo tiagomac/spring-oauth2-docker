f415d372ee2386fc73cedf7950721c33701afdcf
Tiago Mesquita <tiagomac@gmail.com>
2019-05-26 21:42:10 -0300

    fc

---
+++ rserver01/src/main/java/com/tiagomac/Rserver01Application.java
@@ -0,0 +1,13 @@
+package com.tiagomac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rserver01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rserver01Application.class, args);
+	}
+
+}
---
+++ rserver02/src/main/java/com/tiagomac/Rserver02Application.java
@@ -0,0 +1,13 @@
+package com.tiagomac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rserver02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rserver02Application.class, args);
+	}
+
+}
